8a5f179ab82175bbb8c1e689b5be394a9d99a826
Ivan Volkov <ivan.volkov@aimprosoft.com>
2024-06-24 12:50:03 +0300

    initial commit

---
+++ product-service/src/main/java/com/volkov/product_service/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.volkov.product_service.controller;
+
+import com.volkov.product_service.dto.ProductRequest;
+import com.volkov.product_service.dto.ProductResponse;
+import com.volkov.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
